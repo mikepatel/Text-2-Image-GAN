196f1b9a3e24f58abb9a25be2951cac43cb6b8f2
Michael Patel <mikepatel@users.noreply.github.com>
2018-12-21 20:18:50 -0500

    Adding text embedding cnn

--- build_model.py
+++ build_model.py
@@ -8,10 +8,11 @@
 ################################################################################
 # IMPORTs
 import tensorflow as tf
-import tensorflow.layers
+from tensorflow.layers import conv2d, batch_normalization, flatten, dense
 
 
 ################################################################################
+LEAKY_ALPHA = 0.2
 
 # image dimensions: 64x64x3
 IMAGE_ROWS = 64
@@ -20,5 +21,91 @@ IMAGE_CHANNELS = 3
 
 
 ################################################################################
+def my_leaky_relu(tensor):
+    return tf.nn.leaky_relu(tensor, alpha=LEAKY_ALPHA)
+
+
+################################################################################
+# CNN for text embedding
 def build_cnn(image, reuse=False):
-    
+    with tf.variable_scope("cnn") as scope:
+        if reuse:
+            tf.get_variable_scope().reuse_variables()
+
+        #
+        cnn_input = image
+
+        t = conv2d(
+            inputs=cnn_input,
+            filters=64,
+            kernel_size=[4, 4],
+            strides=2,
+            padding="same",
+            activation=my_leaky_relu
+        )
+
+        t = batch_normalization(
+            inputs=t
+        )
+
+        t = conv2d(
+            inputs=t,
+            filters=132,
+            kernel_size=[4, 4],
+            strides=2,
+            padding="same",
+            activation=my_leaky_relu
+        )
+
+        t = batch_normalization(
+            inputs=t
+        )
+
+        t = conv2d(
+            inputs=t,
+            filters=256,
+            kernel_size=[4, 4],
+            strides=2,
+            padding="same",
+            activation=my_leaky_relu
+        )
+
+        t = batch_normalization(
+            inputs=t
+        )
+
+        t = conv2d(
+            inputs=t,
+            filters=512,
+            kernel_size=[4, 4],
+            strides=2,
+            padding="same",
+            activation=my_leaky_relu
+        )
+
+        t = batch_normalization(
+            inputs=t
+        )
+
+        t = flatten(
+            inputs=t
+        )
+
+        t = dense(
+            inputs=t,
+            units=128,
+            activation=None  # linear activation
+        )
+
+        cnn_output = t
+        return cnn_output
+
+
+################################################################################
+# RNN for text embedding
+def build_rnn(reuse=False):
+    with tf.variable_scope("rnn") as scope:
+        if reuse:
+            tf.get_variable_scope().reuse_variables()
+
+        #
