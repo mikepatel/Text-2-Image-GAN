b48a77f45a7b64771facfda4c34f18ccd8afbada
Michael Patel <michael.patel94@gmail.com>
2019-09-24 18:13:01 -0400

    Refactoring

---
+++ parameters.py
@@ -0,0 +1,19 @@
+"""
+Michael Patel
+August 2018
+
+Python 3.6.5
+TensorFlow 1.12.0
+
+Project description:
+    To synthesize images from text-based captions as a form of reverse image captioning.
+
+File description:
+
+Dataset: Oxford-102 flowers
+
+"""
+################################################################################
+# Imports
+
+################################################################################
